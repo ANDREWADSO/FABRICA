3ae5de8c06c49a267c0729f6c4d6d8d105c5d502
ANDREWADSO <financiero.amartinez@gmail.com>
2024-05-18 00:30:15 -0500

    Aprendiz V5

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CustomNavbar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -15,6 +16,7 @@ function App() {
       < Banner />
       < Skills />
       < Projects />
+      < Footer />
 
     </div>
   );
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import { ArrowRightCircle } from "react-bootstrap-icons"
-import headerImg from "../assets/img/header-img.svg"
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
     
@@ -52,10 +54,15 @@ export const Banner = () => {
             <Container>
                 <Row className="aling-items-center">
                     <Col xs={12} md={6} xl={7}>
+                      <TrackVisibility>
+                      {({ isVisible }) =>
+                      <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                         <span className="tagline">¡Bienvenido a Aprendix!</span>
                         <h1>{`Navegaremos`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                        <button onClick={() => console.log('connect')}> Conectate <ArrowRightCircle size={25} /></button>
+                      </div>}
+                      </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img"/>
